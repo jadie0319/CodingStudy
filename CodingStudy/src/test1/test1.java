cf3f4a577889b27357184b72b95f81096fa8aa98
Jaeyong <koola97620@nate.com>
2018-01-07 14:52:26 +0900

    commit test1

---
+++ CodingStudy/src/test1/test1.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Yeah");
+		System.out.println("push test");
+	}
+
+}

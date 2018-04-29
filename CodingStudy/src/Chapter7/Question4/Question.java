dab88071b11a9f1a468102908faa2fad5203408d
Jaeyong <koola97620@nate.com>
2018-04-29 20:27:10 +0900

    04/29/18
     ~ 7-9 Update

--- CodingStudy/src/Chapter7/Question4/Question.java
+++ CodingStudy/src/Chapter7/Question4/Question.java
@@ -1,14 +1,32 @@
 package Chapter7.Question4;
 
-import java.util.Queue;
+import CodeLibrary.AssortedMethods;
 
-
+/*
+ *  7-4 주차장 : 객체 지향 원칙에 따라 주차장을 설계하라.
+ */
 public class Question {
 	public static void main(String[] args) {
+		ParkingLot lot = new ParkingLot();
 		
-		
-		
-		
+		Vehicle v = null;
+		while (v == null || lot.parkVehicle(v)) {
+			lot.print();
+			int r = AssortedMethods.randomIntInRange(0, 10);
+			if (r < 2) {
+				v = new Bus();
+			} else if (r < 4) {
+				v = new Motorcycle();
+			} else {
+				v = new Car();
+			}
+			System.out.print("\nParking a ");
+			v.print();
+			System.out.println("");
+		}
+		System.out.println("Parking Failed. Final state: ");
+		lot.print();
 	}
+		
 
 }

d3ce442be496fdd891967ebc22d551ebbb45a07f
Jaeyong <koola97620@nate.com>
2018-09-23 22:18:29 +0900

    09/21/18
     WebServer update

---
+++ fastcampus/src/examples/MiniWAS/ClassLoaderTest01.java
@@ -0,0 +1,11 @@
+package examples.MiniWAS;
+
+public class ClassLoaderTest01 {
+    public static void main(String[] args) {
+        Sample s1 = new Sample();
+        s1.print();
+        s1.print("hello world");
+
+
+    }
+}
---
+++ fastcampus/src/examples/MiniWAS/Sample.java
@@ -0,0 +1,11 @@
+package examples.MiniWAS;
+
+public class Sample {
+    public void print() {
+        System.out.println("print");
+    }
+
+    public void print(String str) {
+        System.out.println("print : "  + str);
+    }
+}

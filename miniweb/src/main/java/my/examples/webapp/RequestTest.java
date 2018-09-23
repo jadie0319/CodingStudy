3ade77471def4d2957d6476906e38e7bee27c22f
Jaeyong <koola97620@nate.com>
2018-09-23 22:19:14 +0900

    new project

---
+++ miniweb/src/main/java/my/examples/webapp/RequestTest.java
@@ -0,0 +1,9 @@
+package my.examples.webapp;
+
+import examples.MiniWAS.Request;
+
+public class RequestTest {
+    public static void main(String[] args) {
+        Request request = new Request(null,null);
+    }
+}
